9da74f3b65de0bc7a88240746d8edf32099e59c4
soham Kulkarni <soham18262@gmail.com>
2024-04-01 15:31:20 +0530

    buttons added at frontend

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
 
 
 const Home = () => {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <div className="text-2xl " >Welcome to dream Journel</div>
-        
+        <div className="text-4xl " >Welcome to dream Journel</div>
+        <div>Meeting you really soon !</div>
+        <div className="flex flex-row gap-2 ">
+          <Link to={"/signup"} className="bg-black rounded-md text-white p-3 mt-2 hover:bg-slate-700">Signup</Link>
+          <Link to={"/signin"} className="bg-black rounded-md text-white p-3 mt-2 hover:bg-slate-700">Signin</Link>
+          
+        </div>
     </div>
   )
 }
